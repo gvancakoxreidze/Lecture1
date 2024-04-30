90f18612059281449ef74890fa621bfc8d35d6e5
gvancakoxreidze <gvanca04koxreidze@gmail.com>
2024-04-30 16:37:46 +0400

    create commit 1

--- Gvantsak/AppDelegate.swift
+++ Gvantsak/AppDelegate.swift
@@ -12,7 +12,9 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+//hvvcw,ekuvfyculwaiqufcw
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
